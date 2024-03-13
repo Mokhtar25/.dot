da12938ecdedb58845a30a6bbece217f5b97ab54
mohtar25 <mokhtar2000naj@gmail.com>
2024-03-13 11:48:57 +0100

    config files init

---
+++ after/plugin/colors.lua
@@ -0,0 +1,26 @@
+-- require('rose-pine').setup({
+--     disable_background = true
+-- })
+--
+-- function ColorMyPencils(color) 
+-- 	color = color or "rose-pine"
+-- 	vim.cmd.colorscheme(color)
+--
+-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--
+-- end
+--
+-- ColorMyPencils()
+
+-- set colorscheme to nightfly with protected call
+-- in case it isn't installed
+local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- -
+if not status then
+  print("Colorscheme not found!") -- print error if colorscheme not installed
+  return
+end
+
---
+++ lua/main/remap.lua
@@ -0,0 +1,33 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+local keymap = vim.keymap
+--genral keymaps 
+
+keymap.set("n", "x", '"_x')
+
+keymap.set("i", "jk", "<ESC>")
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>") -- increment
+keymap.set("n", "<leader>-", "<C-x>") -- decrement
+
+--delete not into clipbored
+keymap.set("n", "d", '"_d')
+
+--window mangment
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
+
+--- plugins keymaps-- 
+
+
+
+
