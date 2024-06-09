63b519fae14338f4fe850ca73a01a30f56fd8c2f
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-09 23:48:26 +0200

    fixing treesitter

--- lua/main/packer.lua
+++ lua/main/packer.lua
@@ -7,8 +7,6 @@ return require("packer").startup(function(use)
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
 
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-
 	use({
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
@@ -23,13 +21,13 @@ return require("packer").startup(function(use)
 			vim.cmd("colorscheme rose-pine")
 		end,
 	})
-	-- use {
-	--        'nvim-treesitter/nvim-treesitter',
-	--        run = function()
-	--            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-	--            ts_update()
-	--        end,
-	--    }
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 	use("nvim-treesitter/playground")
 
 	use("ThePrimeagen/harpoon")
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -234,14 +234,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: conform.nvim
-time([[Config for conform.nvim]], true)
-try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fconform\frequire\0", "config", "conform.nvim")
-time([[Config for conform.nvim]], false)
 -- Config for: rose-pine
 time([[Config for rose-pine]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
 time([[Config for rose-pine]], false)
+-- Config for: conform.nvim
+time([[Config for conform.nvim]], true)
+try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fconform\frequire\0", "config", "conform.nvim")
+time([[Config for conform.nvim]], false)
 -- Config for: nvim-surround
 time([[Config for nvim-surround]], true)
 try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18nvim-surround\frequire\0", "config", "nvim-surround")
