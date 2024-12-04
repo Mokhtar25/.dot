111f51726dfa22607f6e0f88e47b9f3a88bd84fb
Mokhtar <mokhtar2000naj@gmail.com>
2024-12-04 11:22:05 +0100

    before changing ts lsp

--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -40,6 +40,8 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+keymap.set("n", "<C-Space>", "<cmd> lua vim.lsp.buf.code_action() <CR>")
+
 --- plugins keymaps--
 
 function EscapePair()
--- lua/plugins/importCost.lua
+++ lua/plugins/importCost.lua
@@ -1,7 +1,7 @@
 return {
     {
         'barrett-ruth/import-cost.nvim',
-        build = 'sh install.sh npm', -- Use appropriate command for your OS
+        build = 'sh install.sh npm',
         config = function()
             require('import-cost').setup({
                 -- Filetypes to attach to
