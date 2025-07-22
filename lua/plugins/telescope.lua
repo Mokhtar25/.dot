f3bf4a66a1acab1b0b3fedb43f7e83a88069dd86
Mokhtar <mokhtar2000naj@gmail.com>
2025-07-22 23:55:22 +0200

    adding blink and fzf lua telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,14 +25,14 @@ return {
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fs', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        -- vim.keymap.set('n', '<leader>fs', function()
+        --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        -- end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
         vim.keymap.set("n", "<space>fh", builtin.help_tags)
 
-      -- require "config.telescope.multigrep".setup()
+      require "config.telescopeMultigrep".setup()
     end
   }
 }
