4227729cef8c765aaefa6e29d626e151ae832dd9
Mokhtar <mokhtar2000naj@gmail.com>
2025-08-16 14:57:21 +0200

    maven accept iwth ctrl y

--- lua/plugins/maven.lua
+++ lua/plugins/maven.lua
@@ -4,22 +4,22 @@ return {
         config = function()
             require("supermaven-nvim").setup({
                 keymaps = {
-                    accept_word = "<CR-y>",
+                    accept = "<CR-y>",
                 }
             })
         end,
     },
-    "L3MON4D3/LuaSnip",
-    keys = {
-        { "<tab>",   false, mode = { "i", "s" } },
-        { "<s-tab>", false, mode = { "i", "s" } },
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        keys = {
-            { "<tab>",   false, mode = { "i", "s" } },
-            { "<s-tab>", false, mode = { "i", "s" } },
-        },
-    }
+    -- "L3MON4D3/LuaSnip",
+    -- keys = {
+    --     { "<tab>",   false, mode = { "i", "s" } },
+    --     { "<s-tab>", false, mode = { "i", "s" } },
+    -- },
+    -- {
+    --     "hrsh7th/nvim-cmp",
+    --     keys = {
+    --         { "<tab>",   false, mode = { "i", "s" } },
+    --         { "<s-tab>", false, mode = { "i", "s" } },
+    --     },
+    -- }
 
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,40 +1,40 @@
-
-
 return {
-     {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-    },
-    config = function()
-      require('telescope').setup {
-        pickers = {
-          find_files = {
-            theme = "ivy"
-          }
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
         },
-        extensions = {
-          fzf = {}
-        }
-      }
+        config = function()
+            require('telescope').setup {
+                pickers = {
+                    find_files = {
+                        theme = "ivy"
+                    }
+                },
+                extensions = {
+                    fzf = {
+                    }
+                }
+            }
 
-      require('telescope').load_extension('fzf')
+            require('telescope').load_extension('fzf')
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-        -- vim.keymap.set('n', '<leader>fs', function()
-        --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        -- end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+            -- vim.keymap.set('n', '<leader>fs', function()
+            --     builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            -- end)
+            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
-        vim.keymap.set("n", "<space>fh", builtin.help_tags)
+            vim.keymap.set("n", "<space>fh", builtin.help_tags)
 
-      require "config.telescopeMultigrep".setup()
-    end
-  }
+            require "config.telescopeMultigrep".setup()
+        end
+    }
 }
 --
 -- local builtin = require('telescope.builtin')
