6fb8490790a559df6b81ba085619ec384fd0fc25
Mokhtar <mokhtar2000naj@gmail.com>
2025-08-16 14:37:20 +0200

    tabs with supermaven

--- lua/plugins/maven.lua
+++ lua/plugins/maven.lua
@@ -2,8 +2,24 @@ return {
     {
         "supermaven-inc/supermaven-nvim",
         config = function()
-            require("supermaven-nvim").setup({})
+            require("supermaven-nvim").setup({
+                keymaps = {
+                    accept_word = "<CR-y>",
+                }
+            })
         end,
+    },
+    "L3MON4D3/LuaSnip",
+    keys = {
+        { "<tab>",   false, mode = { "i", "s" } },
+        { "<s-tab>", false, mode = { "i", "s" } },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        keys = {
+            { "<tab>",   false, mode = { "i", "s" } },
+            { "<s-tab>", false, mode = { "i", "s" } },
+        },
     }
 
 }
