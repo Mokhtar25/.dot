aaf0b2a9beac4e5d7bbfe893b8d19efae552d0a9
Mokhtar <mokhtar2000naj@gmail.com>
2026-04-08 11:25:48 +0200

    change default fold close

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -5,37 +5,38 @@
 
 local ok, ts = pcall(require, "nvim-treesitter")
 if not ok then
-	return
+    return
 end
 
 ts.install({
-	"javascript",
-	"typescript",
-	"tsx",
-	"go",
-	"rust",
-	"python",
-	"html",
-	"css",
-	"c",
-	"lua",
-	"vim",
-	"vimdoc",
-	"query",
-	"markdown",
-	"markdown_inline",
+    "javascript",
+    "typescript",
+    "tsx",
+    "jsx",
+    "go",
+    "rust",
+    "python",
+    "html",
+    "css",
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "markdown",
+    "markdown_inline",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	group = vim.api.nvim_create_augroup("user_treesitter", { clear = true }),
-	callback = function(args)
-		local buf = args.buf
-		if not pcall(vim.treesitter.start, buf) then
-			return
-		end
-		-- Treesitter-powered folds and indents (experimental)
-		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-		vim.wo[0][0].foldmethod = "expr"
-		vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-	end,
+    group = vim.api.nvim_create_augroup("user_treesitter", { clear = true }),
+    callback = function(args)
+        local buf = args.buf
+        if not pcall(vim.treesitter.start, buf) then
+            return
+        end
+        -- Treesitter-powered folds and indents (experimental)
+        vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo[0][0].foldmethod = "expr"
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
 })
